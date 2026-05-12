8ff776e1bd1bb2d89ded44333c8d68f585b1ae87
chikki1321golu-cmyk <chikki1321golu@gmail.com>
2026-05-12 15:41:35 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const app = require('./app');
 const { logger } = require('./utils/logger');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   logger.info(`BizGST API running on port ${PORT} [${process.env.NODE_ENV}]`);
